180603a11c7b11cb64c2d9c8dc44dd41b08e938a
Matteo Muscas <thatman.j@gmail.com>
2019-03-26 15:42:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set: session_secret, "poba"
+
+    get '/' do
+      @session = session
+      erb :index
+    end
+
+
 end
